e8b984544b066328ec3109c4e163aaee82a23fdc
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2024-02-27 20:48:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,7 @@ server.use(
   })
 );
 
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 
 
