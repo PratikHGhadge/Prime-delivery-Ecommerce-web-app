765b262c7c82b7c9f4707121ac392217b79186ca
Pratik <93422248+PratikHGhadge@users.noreply.github.com>
2024-02-27 20:40:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,17 @@ server.use(
     origin: process.env.CLIENT_BASE_URL,
   })
 );
+
+server.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
+
+
 // mongodb connection
 connectDB()
   .then(() => {})
