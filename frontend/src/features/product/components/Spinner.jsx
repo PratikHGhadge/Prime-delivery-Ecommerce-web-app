213daf35e05628c2f5becea659394bc39dd333cd
PratikHGhadge <pratikhg.2001@gamil.com>
2024-02-28 19:30:47 +0530

    Spinner updated

--- frontend/src/features/product/components/Spinner.jsx
+++ frontend/src/features/product/components/Spinner.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-screen">
+      <div></div>
       <div className="border-t-4 border-black rounded-full animate-spin h-12 w-12"></div>
     </div>
   );
