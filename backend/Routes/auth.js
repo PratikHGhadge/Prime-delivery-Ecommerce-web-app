07944b4c718dd8351d72ca888ac3dcfb8cdb2e32
Ritesh <riteshkhore@gmail.com>
2024-02-29 15:12:57 +0530

    fix:typo

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -38,7 +38,7 @@ router.get("/google/callback", function (req, res, next) {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: false,
-      sameSite: " lax",
+      sameSite: "lax",
       expires: new Date(Date.now() + 3600000),
     })
     // store the token into cookies and send the response
